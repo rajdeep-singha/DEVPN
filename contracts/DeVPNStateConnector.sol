c7b374f237cf5071c6dd9c560d81a0406cc37851
Rajdeep Singha <137903887+rajdeep-singha@users.noreply.github.com>
2026-01-16 14:53:04 +0530

    Clean up DeVPNStateConnector.sol
    Removed unnecessary whitespace and comments.

--- contracts/DeVPNStateConnector.sol
+++ contracts/DeVPNStateConnector.sol
@@ -1,11 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.33;
 
-
 //  DeVPN State Connector Integration
-
-
-
 interface IStateConnector {
    
     function requestAttestation(bytes calldata data) external;
